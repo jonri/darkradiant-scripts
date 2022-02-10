c5ce5e286694de8c27e5ce250ebda50216c64cb5
jonri <jonri@users.noreply.github.com>
2022-02-10 11:04:25 -0500

    Loot counter now counts both selected and total loot

--- count_loot.py
+++ count_loot.py
@@ -1,10 +1,21 @@
 __commandName__ = 'CountLoot'
-__commandDisplayName__ = 'Count selected loot...'
+__commandDisplayName__ = 'Count Loot'
 
 def execute():
 	import darkradiant as dr
 
-	class LootCounter(dr.SelectionVisitor):
+	class SceneLootCounter(dr.SceneNodeVisitor):
+		loot_sum = 0
+		def pre(self, node):
+			entity = node.getEntity()
+			if not entity.isNull():
+				try:
+					self.loot_sum += int(entity.getKeyValue("inv_loot_value"))
+				except:
+					pass
+			return 1
+
+	class SelectionLootCounter(dr.SelectionVisitor):
 		loot_sum = 0
 		def visit(self, node):
 			entity = node.getEntity()
@@ -14,11 +25,17 @@ def execute():
 				except:
 					pass
 
-	counter = LootCounter()
-	GlobalSelectionSystem.foreachSelected(counter)
+	scene_counter = SceneLootCounter()
+	GlobalSceneGraph.root().traverse(scene_counter)
+	
+	selection_counter = SelectionLootCounter()
+	GlobalSelectionSystem.foreachSelected(selection_counter)
 
-	result = 'Total selected loot: ' + str(counter.loot_sum)
-	GlobalDialogManager.createMessageBox('Count Selected Loot Results', result, dr.Dialog.CONFIRM).run()
+	result = "Total loot: " + str(scene_counter.loot_sum)
+	if selection_counter.loot_sum > 0:
+		result += "\nSelection: " + str(selection_counter.loot_sum)
+	
+	GlobalDialogManager.createMessageBox("Loot Count Results", result, dr.Dialog.CONFIRM).run()
 
 if __executeCommand__:
 	execute()
